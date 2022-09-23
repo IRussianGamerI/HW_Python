7faf571864ea1272054f804ec0438d9b79ed7278
Лев Барабанщиков <levbara1@gmail.com>
2022-09-23 11:04:44 +0300

    Added test for draw and reformatted

--- tests.py
+++ tests.py
@@ -11,28 +11,49 @@ class TicTacGameTests(unittest.TestCase):
         self.assertEqual(TicTacGame([1, 2, 3, 'X', 'X', 'X', 'O', 'O', 9]).check_winner(), 1)
 
     def test_check_winner_3(self):
-        self.assertEqual(TicTacGame([1, 2, 3, 4, 'O', 'O', 'X', 'X', 'X']).check_winner(), 1)
+        self.assertEqual(TicTacGame([1, 2, 3,
+                                     4, 'O', 'O',
+                                     'X', 'X', 'X']).check_winner(), 1)
 
     def test_check_winner_4(self):
-        self.assertEqual(TicTacGame(['X', 2, 'O', 4, 'X', 'O', 7, 8, 'X']).check_winner(), 1)
+        self.assertEqual(TicTacGame(['X', 2, 'O',
+                                     4, 'X', 'O',
+                                     7, 8, 'X']).check_winner(), 1)
 
     def test_check_winner_5(self):
-        self.assertEqual(TicTacGame(['O', 'O', 'X', 4, 'X', 6, 'X', 8, 9]).check_winner(), 1)
+        self.assertEqual(TicTacGame(['O', 'O', 'X',
+                                     4, 'X', 6,
+                                     'X', 8, 9]).check_winner(), 1)
 
     def test_check_winner_6(self):
-        self.assertEqual(TicTacGame(['O', 'O', 'O', 4, 'X', 6, 'X', 8, 9]).check_winner(), 2)
+        self.assertEqual(TicTacGame(['O', 'O', 'O',
+                                     4, 'X', 6,
+                                     'X', 8, 9]).check_winner(), 2)
 
     def test_check_winner_7(self):
-        self.assertEqual(TicTacGame(['X', 2, 'X', 'O', 'O', 'O', 'O', 8, 'X']).check_winner(), 2)
+        self.assertEqual(TicTacGame(['X', 2, 'X',
+                                     'O', 'O', 'O',
+                                     'O', 8, 'X']).check_winner(), 2)
 
     def test_check_winner_8(self):
-        self.assertEqual(TicTacGame(['X', 2, 'X', 4, 'X', 6, 'O', 'O', 'O']).check_winner(), 2)
+        self.assertEqual(TicTacGame(['X', 2, 'X',
+                                     4, 'X', 6,
+                                     'O', 'O', 'O']).check_winner(), 2)
 
     def test_check_winner_9(self):
-        self.assertEqual(TicTacGame(['O', 2, 'X', 4, 'O', 'X', 'X', 8, 'O']).check_winner(), 2)
+        self.assertEqual(TicTacGame(['O', 2, 'X',
+                                     4, 'O', 'X',
+                                     'X', 8, 'O']).check_winner(), 2)
 
     def test_check_winner_10(self):
-        self.assertEqual(TicTacGame(['X', 'X', 'O', 'X', 'O', 6, 'O', 8, 9]).check_winner(), 2)
+        self.assertEqual(TicTacGame(['X', 'X', 'O',
+                                     'X', 'O', 6,
+                                     'O', 8, 9]).check_winner(), 2)
+
+    def test_check_winner_11(self):
+        self.assertEqual(TicTacGame(['X', 'O', 'X',
+                                     'O', 'O', 'X',
+                                     'X', 'X', 'O']).check_winner(), 0)
 
     def test_validate_input_1(self):
         instance = None
@@ -74,7 +95,9 @@ class TicTacGameTests(unittest.TestCase):
         self.assertTrue(TicTacGame().validate_input('4'))
 
     def test_validate_input_6(self):
-        self.assertFalse(TicTacGame(['X', 'X', 'O', 'X', 'O', 6, 'O', 8, 9]).validate_input('4'))
+        self.assertFalse(TicTacGame(['X', 'X', 'O',
+                                     'X', 'O', 6,
+                                     'O', 8, 9]).validate_input('4'))
 
 
 if __name__ == '__main__':
