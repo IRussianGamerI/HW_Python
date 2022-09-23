34895ca392da2f8d2acb94448af71b3eb7563d4e
Лев Барабанщиков <levbara1@gmail.com>
2022-09-23 09:28:18 +0300

    HW1 added

---
+++ main.py
@@ -0,0 +1,74 @@
+class TicTacGame:
+
+    def __init__(self, __cells=None):  # For testing only
+        if __cells is not None:
+            self.cells = __cells
+        else:
+            self.cells = list(range(1, 10))
+
+    def show_board(self):
+        for i in range(0, 7, 3):
+            print('', self.cells[i], '│', self.cells[i + 1],
+                  '│', self.cells[i + 2])
+            if i < 6:
+                print("───┼───┼───")
+
+    def validate_input(self, cell):
+        try:
+            cell = int(cell)
+        except ValueError as exc:
+            raise TypeError("Необходимо ввести целое число") from exc  # Получили на вход не число
+            # => ошибка типа
+        else:
+            if 1 <= cell <= 9:
+                return str(self.cells[cell - 1]) not in "XO"
+            raise IndexError("Вводимое число должно лежать в диапазоне [1, 9]")
+
+    def start_game(self):
+        print("Игра началась!")
+        win_check = False
+        steps_num = 0
+        self.show_board()
+        while not win_check:
+            validation = True
+            while validation:
+                sym = "O" if steps_num % 2 else "X"
+                cell = input("Куда вы хотите поставить " + sym + "? ")
+                try:
+                    check_input = self.validate_input(cell)
+                except TypeError as type_err:
+                    print(type_err.args[0])
+                except IndexError as ind_err:
+                    print(ind_err.args[0])
+
+                else:
+                    if check_input:
+                        self.cells[int(cell) - 1] = sym
+                        validation = False
+                        self.show_board()
+                    else:
+                        print("Клетка занята! В ней уже стоит " + self.cells[int(cell) - 1] + "!")
+
+            steps_num += 1
+            if steps_num >= 5:
+                win_check = self.check_winner()
+                if win_check:
+                    break
+            if steps_num == 9:
+                self.show_board()
+                print("Ничья!")
+                break
+
+    def check_winner(self):
+        combs = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7),
+                 (2, 5, 8), (0, 4, 8), (2, 4, 6))
+        for i in combs:
+            if self.cells[i[0]] == self.cells[i[1]] == self.cells[i[2]]:
+                print(f"Победил игрок {self.cells[i[0]]}!")
+                return 1 if self.cells[i[0]] == "X" else 2
+        return 0
+
+
+if __name__ == "__main__":
+    game = TicTacGame()
+    game.start_game()
---
+++ tests.py
@@ -0,0 +1,77 @@
+import unittest
+from main import TicTacGame
+
+
+class TicTacGameTests(unittest.TestCase):
+
+    def test_check_winner_1(self):
+        self.assertEqual(TicTacGame(['X', 'X', 'X', 4, 'O', 'O', 7, 8, 9]).check_winner(), 1)
+
+    def test_check_winner_2(self):
+        self.assertEqual(TicTacGame([1, 2, 3, 'X', 'X', 'X', 'O', 'O', 9]).check_winner(), 1)
+
+    def test_check_winner_3(self):
+        self.assertEqual(TicTacGame([1, 2, 3, 4, 'O', 'O', 'X', 'X', 'X']).check_winner(), 1)
+
+    def test_check_winner_4(self):
+        self.assertEqual(TicTacGame(['X', 2, 'O', 4, 'X', 'O', 7, 8, 'X']).check_winner(), 1)
+
+    def test_check_winner_5(self):
+        self.assertEqual(TicTacGame(['O', 'O', 'X', 4, 'X', 6, 'X', 8, 9]).check_winner(), 1)
+
+    def test_check_winner_6(self):
+        self.assertEqual(TicTacGame(['O', 'O', 'O', 4, 'X', 6, 'X', 8, 9]).check_winner(), 2)
+
+    def test_check_winner_7(self):
+        self.assertEqual(TicTacGame(['X', 2, 'X', 'O', 'O', 'O', 'O', 8, 'X']).check_winner(), 2)
+
+    def test_check_winner_8(self):
+        self.assertEqual(TicTacGame(['X', 2, 'X', 4, 'X', 6, 'O', 'O', 'O']).check_winner(), 2)
+
+    def test_check_winner_9(self):
+        self.assertEqual(TicTacGame(['O', 2, 'X', 4, 'O', 'X', 'X', 8, 'O']).check_winner(), 2)
+
+    def test_check_winner_10(self):
+        self.assertEqual(TicTacGame(['X', 'X', 'O', 'X', 'O', 6, 'O', 8, 9]).check_winner(), 2)
+
+    def test_validate_input_1(self):
+        instance = None
+        try:
+            TicTacGame().validate_input('qwerty')
+        except TypeError as t_err:
+            instance = t_err
+        finally:
+            self.assertTrue(isinstance(instance, TypeError))
+
+    def test_validate_input_2(self):
+        instance = None
+        try:
+            TicTacGame().validate_input('228')
+        except IndexError as ind_err:
+            instance = ind_err
+        finally:
+            self.assertTrue(isinstance(instance, IndexError))
+
+    def test_validate_input_3(self):
+        instance = None
+        try:
+            TicTacGame().validate_input('228.2')
+        except TypeError as t_err:
+            instance = t_err
+        finally:
+            self.assertTrue(isinstance(instance, TypeError))
+
+    def test_validate_input_4(self):
+        instance = None
+        try:
+            TicTacGame().validate_input('228+322-501ю*Technopark@VKxМГТУ')
+        except TypeError as t_err:
+            instance = t_err
+        finally:
+            self.assertTrue(isinstance(instance, TypeError))
+
+    def test_validate_input_5(self):
+        self.assertTrue(TicTacGame().validate_input('4'))
+
+    def test_validate_input_6(self):
+        self.assertFalse(TicTacGame(['X', 'X', 'O', 'X', 'O', 6, 'O', 8, 9]).validate_input('4'))
