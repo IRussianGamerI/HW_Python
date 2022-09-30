cd10d00c1404dcf3cdc36795caeed4eaa1731743
Лев Барабанщиков <levbara1@gmail.com>
2022-09-30 09:24:47 +0300

    Fixed main.py to satisfy linters

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ def parse_json(keyword_callback, json_str: str, required_fields=None, keywords=N
         keywords = []
     try:
         json_loaded = json.loads(json_str)
-    except json.decoder.JSONDecodeError:
-        raise TypeError('Given str is not JSON-like')
+    except json.decoder.JSONDecodeError as json_exc:
+        raise TypeError('Given str is not JSON-like') from json_exc
     for field in required_fields:
         if field in json_loaded:
             try:
                 keywords_list = json_loaded[field].split(' ')
-            except AttributeError:
-                raise TypeError('Given value is not str')
+            except AttributeError as json_exc:
+                raise TypeError('Given value is not str') from json_exc
             for value in keywords_list:
                 if value in keywords:
                     keyword_callback(field, value)
 
 
 def keyword_callback_example(field, key):
-    print('There is a keyword "{0}" in the field "{1}"'.format(key, field))
+    print(f'There is a keyword "{key}" in the field "{field}"')
