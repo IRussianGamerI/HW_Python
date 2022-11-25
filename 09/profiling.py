51878f3d1bea30c0091a70e8d198e5921496df30
Лев Барабанщиков <levbara1@gmail.com>
2022-11-25 09:56:17 +0300

    Profiling implemented

---
+++ 09/profiling.py
@@ -0,0 +1,36 @@
+import cProfile
+import io
+import pstats
+
+from classes import MyPreciousClass, \
+    MyPreciousClassWithSlots, \
+    MyPreciousClassWithWeakrefs, \
+    StrWrap, \
+    Dict, \
+    List, \
+    process_attrs_in_list, \
+    process_weakrefs_in_list
+
+
+N = 1_000_000
+
+
+if __name__ == "__main__":
+    pr = cProfile.Profile()
+    pr.enable()
+
+    my_precious_list = [MyPreciousClass() for _ in range(N)]
+    my_precious_list_with_slots = [MyPreciousClassWithSlots() for _ in range(N)]
+    my_precious_list_with_weakrefs = [MyPreciousClassWithWeakrefs() for _ in range(N)]
+
+    process_attrs_in_list(my_precious_list)
+    process_attrs_in_list(my_precious_list_with_slots)
+    process_weakrefs_in_list(my_precious_list_with_weakrefs)
+
+    pr.disable()
+
+    out = io.StringIO()
+    ps = pstats.Stats(pr, stream=out)
+    ps.print_stats()
+
+    print(out.getvalue())
